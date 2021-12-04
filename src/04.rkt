2f1a0d25efa186de9c50985548f20c9716aa1e6c
Jonathan Chan <jcxz@cs.ubc.ca>
2021-12-04 00:57:22 -0800

    Day 04: Use entire file instead of splitting input beforehand.

--- src/04.rkt
+++ src/04.rkt
@@ -2,21 +2,22 @@
 
 (require "../lib.rkt")
 
+(define input (problem-input-grouped 4))
+
 (define order
-  (~> (problem-input 4 "-order")
-      first
-      (string-split ",")
-      (map string->number _)))
+  (string-csv (first input) string->number))
 
 (define boards
-  (~> (problem-input-grouped 4)
-      (map string-lines _)
-      (map #{map string-words %} _)
-      (map #{map #{map string->number %} %} _)))
+  (for/list ([board (rest input)])
+    (for/list ([row (string-lines board)])
+      (for/list ([cell (string-words row)])
+        (string->number cell)))))
 
 (define (winner? board)
   (or (ormap #{andmap false? %} board)
-      (ormap #{andmap (λ (row) (false? (list-ref row %))) board} (range 5))))
+      ;; IMO this is the more aesthetic but less efficient version
+      #;(ormap #{andmap false? %} (transpose board))
+      (ormap #{not (ormap (∂r list-ref %) board)} (range (length board)))))
 
 (define (blot board n)
   (for/list ([row board])
@@ -27,36 +28,26 @@
 (define (score board)
   (sum (filter number? (apply append board))))
 
-(define-values (winner n)
+(define part1
   (for/fold ([boards boards]
              [winner #f]
              [winning #f]
-             #:result (values winner winning))
+             #:result (* (score winner) winning))
             ([n order]
              #:break winner)
     (define boards*
-      (for/list ([board boards])
-        (blot board n)))
-    (define winner
-      (findf winner? boards*))
-    (values boards* winner n)))
-
-(define-values (loser m)
-  (for/fold ([boards boards]
-             [losing #f]
-             #:result (values (first boards) losing))
-            ([m order]
-             #:break (and (singleton? boards)
-                          (winner? (first boards))))
-    (define boards*
-      (for/list ([board (filter (∘ not winner?) boards)])
-        (blot board m)))
-    (values boards* m)))
-
-(define part1
-  (* n (score winner)))
+      (map #{blot % n} boards))
+    (values boards* (findf winner? boards*) n)))
 
 (define part2
-  (* m (score loser)))
+  (for/fold ([boards boards]
+             [loser #f]
+             [losing #f]
+             #:result (* (score loser) losing))
+            ([m order]
+             #:break (empty? boards))
+    (define boards*
+      (map #{blot % m} boards))
+    (values (filter (∘ not winner?) boards*) (first boards*) m)))
 
 (show-solution part1 part2)
